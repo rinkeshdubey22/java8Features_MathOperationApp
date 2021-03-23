d8ce775adeb62735db93ad09224d0fd03de580b6
Rinkesh Dubey <rinkeshdubey2@gmail.com>
2021-03-23 12:55:32 +0530

    [Rinkesh] : add Added Lambda Expression to Show Results

--- src/main/java/com/bridgelabz/MathOperationApp.java
+++ src/main/java/com/bridgelabz/MathOperationApp.java
@@ -3,6 +3,9 @@ package com.bridgelabz;
 @FunctionalInterface
 interface IMATHFUNCTION {
     int calculate(int a, int b);
+    static void printResult(int a, int b, String function, IMATHFUNCTION fobj) {
+        System.out.println("Result of " + function + " is " + fobj.calculate(a, b));
+    }
 }
 
 public class MathOperationApp {
@@ -23,6 +26,11 @@ public class MathOperationApp {
         System.out.println("Multiplication of Two Numbers is " + multiplication.calculate(5,4));
         System.out.println("Division of Two Numbers is " + division.calculate(5,4));
 
+        //Passing Lambda Expression As Function parameter To Print Result Using Static Function
+        IMATHFUNCTION.printResult(5, 4, "Addition ", add);
+        IMATHFUNCTION.printResult(5, 4, "Subtraction ", subtraction);
+        IMATHFUNCTION.printResult(5, 4, "Multiplication ", multiplication);
+        IMATHFUNCTION.printResult(5, 4, "Division ", division);
     }
 
 }
